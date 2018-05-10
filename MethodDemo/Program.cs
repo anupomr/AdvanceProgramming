94b93fe7294271f4b09a85dbe34c6e82281fa03a
Anupom Roy <anupomr@yahoo.com>
2018-05-10 16:39:17 -0400

    Rational Demo is created

---
+++ MethodDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayIntArray(new int[] {2, 3, 4, 7, 8, 9 });
+            Console.WriteLine("");
+            var temp = GenerateRandomArray(15, 10);
+            DisplayIntArray(temp);
+            Console.Write("\n\tEven and Odd are ");
+            var count = CountOddEvent(temp);
+            DisplayIntArray(count);
+            Console.WriteLine("");
+        }
+
+        static void DisplayIntArray(int[] numbers)
+        {
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item,3}");
+            }
+
+        }
+        static int[] GenerateRandomArray(int numberofItem, int largestValue)
+        {
+            Random generator = new Random();
+            int[] result = new int[numberofItem];
+            for (int i = 0; i < numberofItem; i++)
+                result[i] = generator.Next(largestValue);
+            return result;
+
+        }
+        static int[] CountOddEvent(int[] array)
+        {
+            int[] result = new int[2];
+            foreach (var item in array)
+            {
+                result[item % 2]++;
+            }
+
+
+            return result;
+        }
+
+    }
+}
---
+++ RationalDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Rational(3, 5));
+            Rational r1 = new Rational(2, 3);
+            Rational r2 = new Rational(4, 7);
+            Rational r3 = new Rational().Add(r2);
+
+            Console.WriteLine($"{r1} + {r2}={r2.Add(r1)}");
+            Console.WriteLine($"{r1+r2}");
+        }
+    }
+    class Rational
+    {
+        public int Numerator { get; private set; }
+        public int Denamerator { get; private set; }
+
+        public Rational(int numerator = 0, int denemator = 1)
+        {
+            Numerator = numerator;
+            Denamerator = denemator;
+        }
+        public override string ToString()
+        => $"<{Numerator} / {Denamerator}>";
+
+        public Rational Add(Rational other)
+        {
+            int denominator = this.Denamerator * other.Denamerator;
+            int numerator = Numerator * other.Denamerator + Denamerator * other.Numerator;
+            return new Rational(numerator, denominator);
+        }
+        public static Rational operator +(Rational hs, Rational rhs) {
+            int denominator = hs.Denamerator * rhs.Denamerator;
+            int numerator = hs.Numerator * rhs.Denamerator + hs.Denamerator * rhs.Numerator;
+            return new Rational(numerator, denominator);
+        }
+    }
+}
