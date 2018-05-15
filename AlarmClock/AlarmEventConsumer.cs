1f38e7a554f78035e76a9a6dd72c75936245a99f
Anupom Roy <anupomr@yahoo.com>
2018-05-15 15:27:45 -0400

    AlarmEventConsumer is created

---
+++ AlarmClock/AlarmEventConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    class AlarmEventConsumer
+    {
+        private string name;
+
+        public AlarmEventConsumer(string name)
+        {
+            this.name = name;
+        }
+        public void AlarmRang(object sender, AlarmEventArgs e)
+        {
+            Console.WriteLine(name+ " was called with "+e.Text);
+        }
+    }
+}
